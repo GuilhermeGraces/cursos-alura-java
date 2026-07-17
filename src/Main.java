d37f9afb495c55d63b990f23cad4d159d5229a2d
Guilherme Graces <guilhermegracesoli@gmail.com>
2026-07-17 16:15:03 -0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("olá, me chamo Guilherme!");
+        System.out.println("tudo bem?");
+    }
+
+}
+
---
+++ src/Soma.java
@@ -0,0 +1,7 @@
+public class Soma {
+    static void main(String[] args) {
+        int num1=5;
+        int num2=6;
+        System.out.println(num1+num2);
+    }
+}
